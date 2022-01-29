47ad2e3b26195e9a3e34ce027eb17d48b33d3f89
Febin <kfebin8@gmail.com>
2022-01-29 23:37:14 +0530

    Second Module. Interactive addition of expenses added. Remove capability not added

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,17 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+// import React, {useState} from 'react';
 
 
 function ExpenseItem(props) {
-  
+
+  // const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    props.onRemove(props.id);
+  };
+  // console.log(props.id)
   return (
     <Card className="expense-item">
           <div>
@@ -14,6 +21,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{'$ ' + props.amount}</div>
       </div>
+      <button className = 'expense-item__delete' onClick={clickHandler}>Remove</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,10 +5,14 @@ import Card from '../UI/Card';
 
 
 function Expenses (props) {
+    const removeHandler = id => {
+        props.onDelete(id);
+    }
     return (
         <Card className='expenses'>
             {props.exp.map(x => {
-                return <ExpenseItem title = {x.title} amount = {x.amount} date = {x.date} />
+                // console.log(x.id);
+                return <ExpenseItem title = {x.title} amount = {x.amount} date = {x.date} id = {x.id} onRemove = {removeHandler} />
             })}
         </Card>
     );
